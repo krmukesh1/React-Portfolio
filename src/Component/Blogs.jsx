d50ee9eb32c14fcf285c82fc21736e0c4fd3419a
email <krmukeshsingh51@gmail.com>
2021-08-29 12:48:46 +0530

    updated codes

--- src/Component/Blogs.jsx
+++ src/Component/Blogs.jsx
@@ -7,7 +7,33 @@ const Blogs = () => {
         <h1 className="hero-heading">
           My<span className="heading-inverted"> Blogs</span>
         </h1>
-        <h1 className="center">COMING SOON....!</h1>
+        <li className="list-item-inline">
+          <h1>Our Career Journey </h1>
+          <p>
+            Everything about our Career Journey as Professional
+            <a
+              className="link"
+              href="https://careertech51.blogspot.com/"
+              target="_blank"
+            >
+              Read more...
+            </a>
+          </p>
+        </li>
+        <li className="list-item-inline">
+          <h1>HTML Syntax </h1>
+          <p>
+            HTML is not a programming language, but can still be considered the
+            most basic building block of the
+            <a
+              className="link"
+              href="https://careertech51.blogspot.com/"
+              target="_blank"
+            >
+              Read more...
+            </a>
+          </p>
+        </li>
       </header>
     </>
   );
